ce3a5710384f1ba8962c86b07aeda7cc4e798c31
sora <103909442+sora-dayo@users.noreply.github.com>
2023-07-11 09:58:12 +0900

    アカウント画面投稿フィルタリング

--- lib/account.dart
+++ lib/account.dart
@@ -1,17 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:omorode/postmap.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({Key? key}) : super(key: key);
 
   @override
-  State<AccountScreen> createState() => _AccountScreenState();
+  State<StatefulWidget> createState() {
+    return _State();
+  }
 }
 
-class _AccountScreenState extends State<AccountScreen> {
+class _State extends State<AccountScreen> {
+  late Stream<QuerySnapshot> _stream;
+  List<Map<String, dynamic>> items = [];
+  late String currentUserUid;
+
+  @override
+  void initState() {
+    super.initState();
+    _stream = FirebaseFirestore.instance.collection('Text').snapshots();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    try {
+      final querySnapshot =
+          await FirebaseFirestore.instance.collection('Text').get();
+      final documents = querySnapshot.docs;
+
+      for (final document in documents) {
+        final uid = document['uid'];
+
+        if (uid != currentUserUid) {
+          continue;
+        }
+
+        final userDoc =
+            await FirebaseFirestore.instance.collection('User').doc(uid).get();
+        final userName = userDoc['UserName'];
+
+        final item = {
+          'coment': document['coment'],
+          'userName': userName,
+          'image': document['image'],
+        };
+
+        items.add(item);
+      }
+
+      setState(() {});
+    } catch (error) {
+      print('エラーが発生しました: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('マップ', style: TextStyle(fontSize: 32.0))),
+    currentUserUid = FirebaseAuth.instance.currentUser?.uid ?? '';
+
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = items[index];
+          return Container(
+            padding:
+                const EdgeInsets.only(left: 5, top: 0, right: 5, bottom: 1),
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${item['userName']}',
+                            style: const TextStyle(
+                                fontSize: 16.0, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 4.0),
+                          Text(
+                            '${item['coment']}',
+                            style: const TextStyle(fontSize: 14.0),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 8.0),
+                    SizedBox(
+                      width: 150,
+                      height: 100,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: item.containsKey('image')
+                            ? Image.network(
+                                '${item['image']}',
+                                fit: BoxFit.contain,
+                              )
+                            : Container(),
+                      ),
+                    ),
+                  ],
+                ),
+                const Divider(
+                  color: Colors.black,
+                  height: 10,
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        activeIcon: Icons.close,
+        backgroundColor: const Color.fromARGB(255, 102, 205, 170),
+        closeManually: true,
+        onPress: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+            return const MapPage();
+          }));
+        },
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.share_rounded),
+            label: '共有',
+            backgroundColor: Colors.blue,
+            onTap: () {},
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.mail),
+            label: 'メール',
+            backgroundColor: Colors.blue,
+            onTap: () {},
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.copy),
+            label: 'コピー',
+            backgroundColor: Colors.blue,
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
