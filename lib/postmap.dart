e4a84ed4e14a57442e0b7734a5b98565d5b01b8b
nobu35 <kd1317104@st.kobedenshi.ac.jp>
2023-07-14 14:12:59 +0900

    searchのタグを一部変更

--- lib/postmap.dart
+++ lib/postmap.dart
@@ -12,10 +12,10 @@ class MapPage extends StatefulWidget {
   MapPageState createState() => MapPageState();
 }
 
-//GoogleMapの表示
 class MapPageState extends State<MapPage> {
   late GoogleMapController mapController;
   final Set<Marker> markers = {};
+  final List<LatLng> selectedPins = []; // 選択されたピンの位置情報を格納するリスト
   late LatLng _initialPosition;
   late bool _loading;
   LatLng? latLng;
@@ -24,27 +24,30 @@ class MapPageState extends State<MapPage> {
     setState(() {
       markers.clear();
       latLng = null;
+      selectedPins.clear(); // 選択されたピンの位置情報をクリア
     });
   }
 
-  //マーカー
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
 
   void onLongPress(LatLng latLng) {
     setState(() {
-      markers.add(Marker(
-        markerId: MarkerId(latLng.toString()),
-        position: latLng,
-      ));
-      print('latLng: $latLng');
+      if (selectedPins.length < 4) { // ピンが4つ未満の場合にのみ追加
+        markers.add(
+          Marker(
+            markerId: MarkerId(latLng.toString()),
+            position: latLng,
+          ),
+        );
+        selectedPins.add(latLng); // 選択されたピンの位置情報を追加
+        print('latLng: $latLng');
+        double latitude = latLng.latitude;
+        double longitude = latLng.longitude;
 
-      double latitude = latLng.latitude;
-      double longitude = latLng.longitude;
-      print('lat.latitude: $latitude');
-      print('lat.longitude: $longitude');
-      this.latLng = latLng;
+        this.latLng = latLng;
+      }
     });
   }
 
@@ -68,59 +71,70 @@ class MapPageState extends State<MapPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.arrow_back,
-                color: Color.fromARGB(255, 102, 205, 170),
-              ),
-              onPressed: () => {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) {
-                  return const App();
-                }))
-              },
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.arrow_back,
+              color: Color.fromARGB(255, 102, 205, 170),
             ),
-            ElevatedButton(
-              onPressed: () {
-                if (latLng != null) {
-                  Navigator.of(context)
-                      .push(MaterialPageRoute(builder: (context) {
-                    return AddItem(latLng: latLng!);
-                  }));
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('座標が選択されていません')),
-                  );
-                }
-              },
-              child: const Text(
-                "次へ",
-              ),
+            onPressed: () => {
+              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                return const App();
+              }))
+            },
+          ),
+          //次へボタン
+          ElevatedButton(
+            onPressed: () {
+              if (latLng != null) {
+                print(selectedPins);
+                Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+                  return AddItem(latLng: latLng!);
+                }));
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('座標が選択されていません')),
+                );
+              }
+            },
+            child: const Text(
+              "次へ",
             ),
-            IconButton(
-              icon: const Icon(Icons.delete),
-              onPressed: deleteMarkers,
-            )
-          ],
-        ),
-        body: _loading
-            ? const CircularProgressIndicator()
-            : SafeArea(
-                child: Stack(fit: StackFit.expand, children: [
-                //googleMap表示プログラム
-                GoogleMap(
-                  onMapCreated: _onMapCreated,
-                  initialCameraPosition: CameraPosition(
-                    target: _initialPosition,
-                    zoom: 17,
+          ),
+          IconButton(
+            icon: const Icon(Icons.delete),
+            //ピンを消す
+            onPressed: deleteMarkers,
+          )
+        ],
+      ),
+      body: _loading
+          ? const CircularProgressIndicator()
+          : SafeArea(
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  GoogleMap(
+                    onMapCreated: _onMapCreated,
+                    initialCameraPosition: CameraPosition(
+                      target: _initialPosition,
+                      zoom: 17,
+                    ),
+                    markers: markers,
+                    myLocationEnabled: true,
+                    myLocationButtonEnabled: true,
+                    onLongPress: onLongPress,
                   ),
-                  markers: markers,
-                  myLocationEnabled: true,
-                  myLocationButtonEnabled: true,
-                  onLongPress: onLongPress,
-                ),
-              ])));
+                ],
+              ),
+            ),
+    );
   }
 }
+
+
+
+
+
+
